756ed68296ee46292e47d0e971bfa7b7e987d664
DemingLi <1335188820@qq.com>
2017-11-26 17:05:35 +0800

    优化增加人物界面

--- app/src/main/java/com/example/lixiang/threekingdoms/DetailActivity.java
+++ app/src/main/java/com/example/lixiang/threekingdoms/DetailActivity.java
@@ -1,6 +1,7 @@
 package com.example.lixiang.threekingdoms;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
